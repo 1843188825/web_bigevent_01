aa797943caf621eb72b5f36ca34ab607ec6eee03
yp <1843188825@qq.com>
2021-01-21 20:56:15 +0800

    文章管理功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,7 +1,8 @@
 // 在发送ajax（） get（） post（） 之前会触发
+var baseURL = 'http://api-breakingnews-web.itheima.net'
 $.ajaxPrefilter(function (options) {
     // alert(options.url)
-    options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    options.url = baseURL + options.url;
     // alert(options.url);
 
 
@@ -13,7 +14,7 @@ $.ajaxPrefilter(function (options) {
     }
     // 3.拦截所有的响应，判断身份认证信息
     options.complete = function (res) {
-        console.log(res.responseJSON);
+        // console.log(res.responseJSON);
         var obj = res.responseJSON;
         if (obj.status == 1 && obj.message === "身份认证失败！") {
             // 销毁token
